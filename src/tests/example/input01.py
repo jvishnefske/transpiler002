b3582c4889d1d44cf2d85a6d76ca1b5155f5f529
John Vishnefske <john.vishnefske@cnhind.com>
2023-11-28 10:54:25 -0600

    add tooling

---
+++ src/tests/example/input01.py
@@ -0,0 +1,16 @@
+# file to parse, and attempt to create python
+import ctypes
+
+
+def square(a: ctypes.c_int32) -> ctypes.c_int32:
+    return a * a
+
+
+def main():
+    b = 1
+    b = b + 1
+    print(square(2))
+
+
+if __name__ == '__main__':
+    main()
